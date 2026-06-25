fdcbce5ce28a715f0af0e46aca04c1e1239fcbf5
Rebeee-lol <rebeca.arce.u@gmail.com>
2026-06-24 23:15:07 -0400

    Add files via upload
    CAMBIOS REINICIO Y VIDAS

--- PusheenSweetCrossing/Pusheen.java
+++ PusheenSweetCrossing/Pusheen.java
@@ -1,56 +1,73 @@
 package edu.upb.lp.game.PusheenSweetCrossing;
 
 public class Pusheen {
-    private PusheenGame game;
-    private int direccion = 0;
-    private int filas = 3;
-    private int columnas = 7;
+	private PusheenGame game;
+	private int direccion = 0; // 0= arriba,1= derecha, 2= abajo, 3= izquierda
+	private int filas = 3;
+	private int columnas = 7;
+	private int vidas = 3;
 
-    public Pusheen(PusheenGame game) {
-        this.game = game;
-    }
+	public Pusheen(PusheenGame game) {
+		this.game = game;
+	}
 
-    public void moverArriba() {
-    }
+	public void moverArriba() {
+	}
 
-    public void moverAbajo() {
-    }
+	public void moverAbajo() {
 
-    public void moverDerecha() {
-        this.game.borrarPusheen(filas, columnas);
-        if (columnas + 1 < 15) {
-            columnas++;
-            direccion = 1;
-        }
-        this.game.dibujarPusheen(filas, columnas, direccion);
-    }
+	}
 
-    public void moverIzquierda() {
-        this.game.borrarPusheen(filas, columnas);
-        if (columnas - 1 >= 0) {
-            columnas--;
-            direccion = 3;
-        }
-        this.game.dibujarPusheen(filas, columnas, direccion);
-    }
+	public void moverDerecha() {
+		game.borrarPusheen(filas, columnas);
+		if (columnas + 1 < 15) {
+			columnas++;
+			direccion = 1;
+		}
+		game.dibujarPusheen(filas, columnas, direccion);
+	}
 
-    public void reiniciarPosicion() {
-        this.game.borrarPusheen(filas, columnas);
-        filas = 3;
-        columnas = 7;
-        direccion = 0;
-        this.game.dibujarPusheen(filas, columnas, direccion);
-    }
+	public void moverIzquierda() {
+		game.borrarPusheen(filas, columnas);
+		if (columnas - 1 >= 0) {
+			columnas--;
+			direccion = 3;
+		}
+		game.dibujarPusheen(filas, columnas, direccion);
+	}
 
-    public int getFilas() {
-        return this.filas;
-    }
+	public int getFilas() {
+		return filas;
+	}
 
-    public int getColumnas() {
-        return this.columnas;
-    }
+	public int getColumnas() {
+		return columnas;
+	}
 
-    public int getDireccion() {
-        return this.direccion;
-    }
+	public int getDireccion() {
+		return direccion;
+	}
+
+	public void reiniciarPosicion() {
+		game.borrarPusheen(filas, columnas);
+		filas = 3;
+		columnas = 7;
+		direccion = 0;
+		game.dibujarPusheen(filas, columnas, direccion);
+
+	}
+	
+	public void perderVida() {
+		vidas--;
+	}
+	
+	public int getVidas() {
+		return vidas;
+	}
+	
+	public void setVidas() {
+		this.vidas=3;
+	}
 }
+
+
--- PusheenSweetCrossing/PusheenGame.java
+++ PusheenSweetCrossing/PusheenGame.java
@@ -1,174 +1,188 @@
 package edu.upb.lp.game.PusheenSweetCrossing;
+
 import java.util.Random;
 
 public class PusheenGame {
-  private PusheenSweetCrossingController controller;
-  private Pusheen pusheen = new Pusheen(this);
-  private Random random = new Random();
-  private String[][] mapaFondos = new String[7][15];
-  private Auto[] listaAutos = new Auto[100];
+	private PusheenSweetCrossingController controller;
+	private Pusheen pusheen = new Pusheen(this);
+	private Random random = new Random();
+	private String[][] mapaFondos = new String[7][15];
+	private Auto[] listaAutos = new Auto[100];
 
-  public PusheenGame(PusheenSweetCrossingController controller) {
-   this.controller = controller;
-   this.pusheen = new Pusheen(this);
-   inicializarMapaInicial();
-  }
+	public PusheenGame(PusheenSweetCrossingController controller) {
+		this.controller = controller;
+		this.pusheen = new Pusheen(this);
+		inicializarMapaInicial();
+	}
 
-  public void moverArriba() {
-   avanzarMundo();
-   verificarColision();
-  }
-  
-  public void moverAbajo() {
-  }
-  
-  public void moverDerecha() {
-   pusheen.moverDerecha();
-   verificarColision();
-  }
-  
-  public void moverIzquierda() {
-   pusheen.moverIzquierda();
-   verificarColision();
-  }
-  
-  public void inicializarMapaInicial() {
-      for (int r = 0; r < 7; r++) {
-          for (int c = 0; c < 15; c++) {
-              int numeroFila = (r % 7) + 1;
-              int numeroColumna = c + 1;
-              mapaFondos[r][c] = "PastoB_r" + numeroFila + "_c" + numeroColumna;
-          }
-      }
-      for (int i = 0; i < 100; i++) {
-          listaAutos[i] = null;
-      }
-  }
-  
-  public void avanzarMundo() {
-       for (int r = 6; r > 0; r--) {
-           for (int c = 0; c < 15; c++) {
-               mapaFondos[r][c] = mapaFondos[r-1][c];
-           }
-       }
-       for (int i = 0; i < 100; i++) {
-           if (listaAutos[i] != null) {
-               listaAutos[i].bajarFila();
-               if (listaAutos[i].getFilas() >= 7) {
-                   listaAutos[i] = null;
-               }
-           }
-       }
-       int tipoFila = random.nextInt(3); 
-       
-       if (tipoFila == 0) {
-           int numeroFilaPasto = random.nextInt(4) + 1;
-           for (int c = 0; c < 15; c++) {
-               mapaFondos[0][c] = "PastoB_r" + numeroFilaPasto + "_c" + (c + 1);
-           }
-       } else if (tipoFila == 1) {
-           for (int c = 0; c < 15; c++) {
-               mapaFondos[0][c] = "Carretera_r4_c" + (c + 1);
-           }
-           Auto nuevoAuto = crearAutoAleatorio(0, 0, 0);
-           agregarAutoALista(nuevoAuto);
-       } else {
-           for (int c = 0; c < 15; c++) {
-               mapaFondos[0][c] = "Carretera_r4_c" + (c + 1) + "_i";
-           }
-           Auto nuevoAuto = crearAutoAleatorio(1, 0, 14);
-           agregarAutoALista(nuevoAuto);
-       }
-       controller.actualizarTodoElMapa();
-   }
-  
-  public Auto crearAutoAleatorio(int direccion, int fila, int columna) {
-      int modelo = random.nextInt(4); 
-      String sufijo = "D";
-      
-      if (direccion == 0) {
-          sufijo = "D";
-      } else {
-          sufijo = "I";
-      }
-      
-      String imagen = "PANQUEQUECAR";
-      if (modelo == 0) {
-          imagen = "CAMIOND-" + sufijo;   
-      } else if (modelo == 1) {
-          imagen = "CAMIONHB-" + sufijo;  
-      } else if (modelo == 2) {
-          imagen = "CAMIONH-" + sufijo;   
-      }else {
-    	  imagen = "PANQUEQUECAR";
-      }
-      
-      return new Auto(imagen, direccion, fila, columna);
-  }
-  
-  private void agregarAutoALista(Auto nuevoAuto) {
-      for (int i = 0; i < 100; i++) {
-          if (listaAutos[i] == null) {
-              listaAutos[i] = nuevoAuto;
-              break;
-          }
-      }
-  }
-  
-  public void moverCarrosManualmente() {
-      for (int i = 0; i < 100; i++) {
-          if (listaAutos[i] != null) {
-              listaAutos[i].mover();
-              if (listaAutos[i].getColumnas() < 0 || listaAutos[i].getColumnas() >= 15) {
-                  listaAutos[i] = null;
-              }
-          }
-      }
-      verificarColision();
-      controller.actualizarTodoElMapa();
-  }
-  
-  public void verificarColision() {
-      int pusheenFila = pusheen.getFilas();
-      int pusheenColumna = pusheen.getColumnas();
-      
-      for (int i = 0; i < 100; i++) {
-          if (listaAutos[i] != null) {
-              if (listaAutos[i].getFilas() == pusheenFila && listaAutos[i].getColumnas() == pusheenColumna) {
-            	  
-                      pusheen.reiniciarPosicion();
-                  }
-              }
-          }
-      }
-  
-  
-  public String getImagenFondo(int f, int c) {
-      return mapaFondos[f][c];
-  }
+	public void moverArriba() {
+		avanzarMundo();
+		verificarColision();
+	}
 
-  public String getImagenObjeto(int f, int c) { 
-      for (int i = 0; i < 100; i++) {
-          if (listaAutos[i] != null) {
-              if (listaAutos[i].getFilas() == f) {
-                  if (listaAutos[i].getColumnas() == c) {
-                      return listaAutos[i].getImagen();
-                  }
-              }
-          }
-      }
-      return null; 
-  }
+	public void moverAbajo() {
+	}
 
-  public Pusheen getPusheen() { 
-      return pusheen; 
-  }
+	public void moverDerecha() {
+		pusheen.moverDerecha();
+		verificarColision();
+	}
 
-  public void borrarPusheen(int filas, int columnas) { 
-      controller.borrarPusheen(filas, columnas); 
-  }
+	public void moverIzquierda() {
+		pusheen.moverIzquierda();
+		verificarColision();
+	}
 
-  public void dibujarPusheen(int filas, int columnas, int direccion) { 
-      controller.dibujarPusheen(filas, columnas, direccion); 
-  }
+	public void inicializarMapaInicial() {
+		for (int r = 0; r < 7; r++) {
+			for (int c = 0; c < 15; c++) {
+				int numeroFila = (r % 7) + 1;
+				int numeroColumna = c + 1;
+				mapaFondos[r][c] = "PastoB_r" + numeroFila + "_c" + numeroColumna;
+			}
+		}
+		for (int i = 0; i < 100; i++) {
+			listaAutos[i] = null;
+		}
+	}
+
+	public void avanzarMundo() {
+		for (int r = 6; r > 0; r--) {
+			for (int c = 0; c < 15; c++) {
+				mapaFondos[r][c] = mapaFondos[r - 1][c];
+			}
+		}
+		for (int i = 0; i < 100; i++) {
+			if (listaAutos[i] != null) {
+				listaAutos[i].bajarFila();
+				if (listaAutos[i].getFilas() >= 7) {
+					listaAutos[i] = null;
+				}
+			}
+		}
+		int tipoFila = random.nextInt(3);
+
+		if (tipoFila == 0) {
+			int numeroFilaPasto = random.nextInt(4) + 1;
+			for (int c = 0; c < 15; c++) {
+				mapaFondos[0][c] = "PastoB_r" + numeroFilaPasto + "_c" + (c + 1);
+			}
+		} else if (tipoFila == 1) {
+			for (int c = 0; c < 15; c++) {
+				mapaFondos[0][c] = "Carretera_r4_c" + (c + 1);
+			}
+			Auto nuevoAuto = crearAutoAleatorio(0, 0, 0);
+			agregarAutoALista(nuevoAuto);
+		} else {
+			for (int c = 0; c < 15; c++) {
+				mapaFondos[0][c] = "Carretera_r4_c" + (c + 1) + "_i";
+			}
+			Auto nuevoAuto = crearAutoAleatorio(1, 0, 14);
+			agregarAutoALista(nuevoAuto);
+		}
+		controller.actualizarTodoElMapa();
+	}
+
+	public Auto crearAutoAleatorio(int direccion, int fila, int columna) {
+		int modelo = random.nextInt(4);
+		String sufijo = "D";
+
+		if (direccion == 0) {
+			sufijo = "D";
+		} else {
+			sufijo = "I";
+		}
+
+		String imagen = "PANQUEQUECAR";
+		if (modelo == 0) {
+			imagen = "CAMIOND-" + sufijo;
+		} else if (modelo == 1) {
+			imagen = "CAMIONHB-" + sufijo;
+		} else if (modelo == 2) {
+			imagen = "CAMIONH-" + sufijo;
+		} else {
+			imagen = "PANQUEQUECAR";
+		}
+
+		return new Auto(imagen, direccion, fila, columna);
+	}
+
+	public void agregarAutoALista(Auto nuevoAuto) {
+		for (int i = 0; i < 100; i++) {
+			if (listaAutos[i] == null) {
+				listaAutos[i] = nuevoAuto;
+				break;
+			}
+		}
+	}
+
+	public void moverCarrosManualmente() {
+		for (int i = 0; i < 100; i++) {
+			if (listaAutos[i] != null) {
+				listaAutos[i].mover();
+				if (listaAutos[i].getColumnas() < 0 || listaAutos[i].getColumnas() >= 15) {
+					listaAutos[i] = null;
+				}
+			}
+		}
+		verificarColision();
+		controller.actualizarTodoElMapa();
+	}
+
+	public void verificarColision() {
+		int pusheenFila = pusheen.getFilas();
+		int pusheenColumna = pusheen.getColumnas();
+		
+
+		for (int i = 0; i < 100; i++) {
+			if (listaAutos[i] != null) {
+				if (listaAutos[i].getFilas() == pusheenFila && listaAutos[i].getColumnas() == pusheenColumna) {
+					pusheen.perderVida();
+					pusheen.reiniciarPosicion();
+					controller.actualizarTodoElMapa();
+					if (pusheen.getVidas()==0) {
+						controller.perdiste();
+					}
+				}
+			}
+		}
+	}
+
+	public String getImagenFondo(int f, int c) {
+		return mapaFondos[f][c];
+	}
+
+	public String getImagenObjeto(int f, int c) {
+		for (int i = 0; i < 100; i++) {
+			if (listaAutos[i] != null) {
+				if (listaAutos[i].getFilas() == f && listaAutos[i].getColumnas() == c) {
+						return listaAutos[i].getImagen();
+					}
+				}
+			}
+		return null;
+	}
+	
+
+	public Pusheen getPusheen() {
+		return pusheen;
+	}
+
+	public void borrarPusheen(int filas, int columnas) {
+		controller.borrarPusheen(filas, columnas);
+	}
+
+	public void dibujarPusheen(int filas, int columnas, int direccion) {
+		controller.dibujarPusheen(filas, columnas, direccion);
+	}
+
+	public int getVidas() {
+		return pusheen.getVidas();
+	}
+	
+	public void reiniciarJuego() {
+		inicializarMapaInicial();
+		pusheen.reiniciarPosicion();
+		pusheen.setVidas();
+	}
 }
